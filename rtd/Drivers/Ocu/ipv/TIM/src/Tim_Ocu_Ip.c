25faa2fc5718410d810fdd5634f75a411f8be80f
cavoicodon <hieudm.qptb@gmail.com>
2022-08-02 22:11:02 +0700

    Remove unnecessarry files, add template files

---
+++ rtd/Drivers/Ocu/ipv/TIM/src/Tim_Ocu_Ip.c
@@ -0,0 +1,8 @@
+/*
+ * Tim_Ocu_Ip.c
+ *
+ *  Created on: Aug 2, 2022
+ *      Author: 20161505
+ */
+
+
---
+++ rtd/Drivers/Ocu/ipv/TIM/src/Tim_Ocu_Irq.c
@@ -0,0 +1,8 @@
+/*
+ * Tim_Ocu_Irq.c
+ *
+ *  Created on: Aug 2, 2022
+ *      Author: 20161505
+ */
+
+
